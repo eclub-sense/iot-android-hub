32e783846e8ffcab1ea787df7232a867fc8a45b3
Pavel Valach <valacpav@fel.cvut.cz>
2015-07-29 16:16:31 +0200

    Hmm, it seems like it doesn't have proper paths

--- app/src/main/assets/www/jxcore/app.js
+++ app/src/main/assets/www/jxcore/app.js
@@ -1,3 +1,27 @@
+var fs = require('fs');
+
+try {
+// requiring a node module
+  var jsnice = require('json-nice');
+
+//using it
+  var obj = {a: 1, b: 2};
+  console.log(jsnice(obj));
+} catch(e) {
+  console.error("Seems like you didn't copy node_modules folder from sample/jxcore");
+}
+
+// execpath
+console.log("execPath", process.execPath);
+
+// cwd
+console.log("process.cwd", process.cwd());
+
+// iOS user directory
+console.log("userPath", fs.readdirSync(process.userPath));
+
+
+// ---- THIS DID NOT WORK
 var zetta = require('zetta');
 
 zetta
