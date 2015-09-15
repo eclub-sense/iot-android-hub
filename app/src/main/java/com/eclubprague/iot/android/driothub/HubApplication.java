dcef9f6a9c9212ab4a3bfa344bbd073b0d2809db
Pavel Valach <valach.pavel@gmail.com>
2015-09-15 16:20:20 +0200

    Added ongoing notification for beacons nearby

--- app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
+++ app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
@@ -1,6 +1,8 @@
 package com.eclubprague.iot.android.driothub;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.RemoteException;
 import android.util.Log;
@@ -27,6 +29,9 @@ public class HubApplication extends Application implements BootstrapNotifier, Ra
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "App started up");
+
+        buildOngoingNotification();
+
         BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
         // To detect proprietary beacons, you must add a line like below corresponding to your beacon
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
@@ -74,10 +79,45 @@ public class HubApplication extends Application implements BootstrapNotifier, Ra
         for(Beacon beacon : collection) {
             Log.d(TAG, "Beacon in range: " + beacon.getBluetoothAddress() + ", " + beacon.getId1() + "," + beacon.getId2());
         }
+
+        modifyNotificationForBeacons(collection);
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
+        destroyOngoingNotification();
+    }
+
+    public void buildOngoingNotification() {
+        Notification notification = new Notification.Builder(this)
+                    .setContentTitle("Beacons in range")
+                    .setContentText("They will show here").setSmallIcon(R.drawable.notification_template_icon_bg)
+                    .setOngoing(true)
+                    .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void modifyNotificationForBeacons(Collection<Beacon> beacons) {
+        StringBuilder sb = new StringBuilder();
+        for(Beacon b : beacons) {
+            sb.append(Double.toString(b.getDistance()) + "m - " + b.getBluetoothAddress() + "\n");
+        }
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Beacons in range")
+                .setContentText(sb.toString()).setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setOngoing(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void destroyOngoingNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(0);
     }
 }
