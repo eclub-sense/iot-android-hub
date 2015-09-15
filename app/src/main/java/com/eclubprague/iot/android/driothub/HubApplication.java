7ee3de0c0f642e0674cf16ea07ff886f9960b7e9
Pavel Valach <valach.pavel@gmail.com>
2015-09-15 15:50:34 +0200

    Bootstrap region for automatic detection of nearby beacons and ranging implemented
    does not work very good yet

--- app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
+++ app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
@@ -2,18 +2,24 @@ package com.eclubprague.iot.android.driothub;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.RemoteException;
 import android.util.Log;
 
 import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
+import java.util.Collection;
+
 /**
  * Created by paulos on 27. 8. 2015.
  */
-public class HubApplication extends Application implements BootstrapNotifier {
+public class HubApplication extends Application implements BootstrapNotifier, RangeNotifier {
     private static final String TAG = ".HubApplication";
     private RegionBootstrap regionBootstrap;
 
@@ -24,33 +30,54 @@ public class HubApplication extends Application implements BootstrapNotifier {
         BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
         // To detect proprietary beacons, you must add a line like below corresponding to your beacon
         // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        // beaconManager.getBeaconParsers().add(new BeaconParser().
+        // EDDYSTONE beacons
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
 
-        // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
+        beaconManager.setRangeNotifier(this);
+        beaconManager.setForegroundScanPeriod(5000);
+        beaconManager.setBackgroundScanPeriod(5000);
+        beaconManager.setBackgroundBetweenScanPeriod(3000);
+        beaconManager.setForegroundBetweenScanPeriod(3000);
+
         Region region = new Region("com.eclubprague.iot.android.driothub.boostrapRegion", null, null, null);
         regionBootstrap = new RegionBootstrap(this, region);
     }
 
     @Override
     public void didEnterRegion(Region region) {
-        Log.d(TAG, "Got a didEnterRegion call");
-        // This call to disable will make it so the activity below only gets launched the first time a beacon is seen (until the next time the app is launched)
-        // if you want the Activity to launch every single time beacons come into view, remove this call.
-        regionBootstrap.disable();
-        Intent intent = new Intent(this, MainActivity.class);
-        // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
-        // created when a user launches the activity manually and it gets launched from here.
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.startActivity(intent);
+        // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
+        try {
+            BeaconManager.getInstanceForApplication(this).startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void didExitRegion(Region region) {
-        // nothing yet
+        try {
+            BeaconManager.getInstanceForApplication(this).stopRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void didDetermineStateForRegion(int i, Region region) {
         // nothing yet
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+        Log.d(TAG, "Got a didRangeBeaconsInRegion call");
+        for(Beacon beacon : collection) {
+            Log.d(TAG, "Beacon in range: " + beacon.getBluetoothAddress() + ", " + beacon.getId1() + "," + beacon.getId2());
+        }
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
     }
 }
