b1a57c5056561c198ff0c7871cc7fd539107f2e3
Pavel Valach <valach.pavel@gmail.com>
2015-09-17 16:45:55 +0200

    Background scanning of beacons is now in a separate service

--- app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
+++ app/src/main/java/com/eclubprague/iot/android/driothub/HubApplication.java
@@ -25,12 +25,9 @@ import java.util.Set;
 /**
  * Created by paulos on 27. 8. 2015.
  */
-public class HubApplication extends Application implements BootstrapNotifier, RangeNotifier {
+public class HubApplication extends Application {
     private static final String TAG = ".HubApplication";
-    private RegionBootstrap regionBootstrap;
     private BackgroundPowerSaver backgroundPowerSaver;
-
-    private Set<Beacon> detectedBeacons = new HashSet<>();
 
     @Override
     public void onCreate() {
@@ -40,96 +37,5 @@ public class HubApplication extends Application implements BootstrapNotifier, Ra
         // Simply constructing this class and holding a reference to it in your custom Application class
         // enables auto battery saving of about 60%
         backgroundPowerSaver = new BackgroundPowerSaver(this);
-
-        buildOngoingNotification();
-
-
-
-        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-        // EDDYSTONE beacons
-        beaconManager.getBeaconParsers().add(new BeaconParser()
-                .setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
-
-        // wake up the app when any beacon is seen (we can specify specific id filers in the parameters below)
-        Region region = new Region("driothub.boostrapRegion", null, null, null);
-        regionBootstrap = new RegionBootstrap(this, region);
-
-        beaconManager.setBackgroundScanPeriod(5000);
-        beaconManager.setBackgroundBetweenScanPeriod(15000);
-
-        Log.d(TAG, "Started monitoring for target region");
-    }
-
-
-
-    @Override
-    public void didEnterRegion(Region region) {
-        Log.d(TAG, "Got a didEnterRegion call");
-    }
-
-    @Override
-    public void didExitRegion(Region region) {
-        Log.d(TAG, "Got a didExitRegion call");
-
-    }
-
-    @Override
-    public void didDetermineStateForRegion(int i, Region region) {
-        // nothing yet
-    }
-
-    @Override
-    public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
-        Log.d(TAG, "Got a didRangeBeaconsInRegion call");
-        detectedBeacons.clear();
-        for(Beacon beacon : collection) {
-            Log.d(TAG, "Beacon in range: " + beacon.getBluetoothAddress() + ", " + beacon.getId1() + "," + beacon.getId2());
-            detectedBeacons.add(beacon);
-        }
-
-        modifyNotificationForBeacons(detectedBeacons);
-    }
-
-    @Override
-    public void onTerminate() {
-        super.onTerminate();
-        destroyOngoingNotification();
-    }
-
-    public void buildOngoingNotification() {
-        Notification notification = new Notification.Builder(this)
-                    .setContentTitle("Beacons in range")
-                    .setContentText("They will show here").setSmallIcon(R.drawable.notification_template_icon_bg)
-                    .setOngoing(true)
-                    .build();
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification);
-    }
-
-    public void modifyNotificationForBeacons(Collection<Beacon> beacons) {
-        StringBuilder sb = new StringBuilder();
-        for(Beacon b : beacons) {
-            sb.append(String.format("%.2f m - %s%n", b.getDistance(), b.getBluetoothAddress()));
-        }
-        if(sb.toString().isEmpty()){
-            sb.append("No beacons in range");
-        }
-
-        Notification notification = new Notification.Builder(this)
-                .setContentTitle("Beacons in range")
-                .setContentText(sb.toString()).setSmallIcon(R.drawable.notification_template_icon_bg)
-                .setOngoing(true)
-                .build();
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(0, notification);
-    }
-
-    public void destroyOngoingNotification() {
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.cancel(0);
     }
 }
---
+++ app/src/main/java/com/eclubprague/iot/android/driothub/services/BeaconMonitorService.java
@@ -0,0 +1,133 @@
+package com.eclubprague.iot.android.driothub.services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+
+import com.eclubprague.iot.android.driothub.R;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by paulos on 17. 9. 2015.
+ */
+public class BeaconMonitorService extends Service implements RangeNotifier, BeaconConsumer {
+
+    protected final String TAG = "BeaconMonitorService";
+    protected Region region;
+
+    private Set<Beacon> detectedBeacons = new HashSet<>();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+        // EDDYSTONE beacons
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19"));
+
+        beaconManager.setBackgroundScanPeriod(5000);
+        beaconManager.setBackgroundBetweenScanPeriod(15000);
+
+        beaconManager.setRangeNotifier(this);
+        beaconManager.bind(this);
+
+        Log.d(TAG, "Started ranging for target region");
+    }
+
+    @Override
+    public void onDestroy() {
+        if(region != null) {
+            try {
+                BeaconManager.getInstanceForApplication(this)
+                        .stopRangingBeaconsInRegion(region);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.d(TAG, "Finished ranging for target region");
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        // wake up the app when any beacon is seen (we can specify specific id filers in the parameters below)
+        region = new Region("driothub.beacons", null, null, null);
+        try {
+            BeaconManager.getInstanceForApplication(this)
+                    .startRangingBeaconsInRegion(region);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+        Log.d(TAG, "Got a didRangeBeaconsInRegion call");
+        detectedBeacons.clear();
+        for(Beacon beacon : collection) {
+            Log.d(TAG, "Beacon in range: " + beacon.getBluetoothAddress() + ", " + beacon.getId1() + "," + beacon.getId2());
+            detectedBeacons.add(beacon);
+        }
+
+        modifyNotificationForBeacons(detectedBeacons);
+    }
+
+
+    /* Notifications in the action center
+    ---------------- */
+
+    public void buildOngoingNotification() {
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Beacons in range")
+                .setContentText("They will show here").setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setOngoing(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void modifyNotificationForBeacons(Collection<Beacon> beacons) {
+        StringBuilder sb = new StringBuilder();
+        for(Beacon b : beacons) {
+            sb.append(String.format("%.2f m - %s%n", b.getDistance(), b.getBluetoothAddress()));
+        }
+        if(sb.toString().isEmpty()){
+            sb.append("No beacons in range");
+        }
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("Beacons in range")
+                .setContentText(sb.toString()).setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setOngoing(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    public void destroyOngoingNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(0);
+    }
+}
